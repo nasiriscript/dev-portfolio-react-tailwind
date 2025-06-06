c4a146f75dbe23cf17654194e2d7217274914d2e
Nasiri <nasiriscript@gmail.com>
2025-06-06 17:33:23 +0330

    ui

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -44,7 +44,7 @@ export const Hero = () => {
             transition={{ duration: 0.8 }}
             className="max-w-2xl mx-auto"
           >
-            <h1 className="text-6xl md:text-7xl font-bold tracking-tighter mb-28">
+            <h1 className="text-6xl md:text-7xl font-bold tracking-tighter mb-24">
               Hi, We are <br /> Assistio{" "}
               <motion.span
   initial={{ filter: "drop-shadow(0px 0px 4px rgba(34,197,94,0.4))" }}
@@ -66,7 +66,7 @@ export const Hero = () => {
   AI
 </motion.span>
             </h1>
-            <p className="text-xl text-white/80 max-w-lg mx-auto leading-relaxed mb-12 text-shadow-lg drop-shadow-[0_0px_2px_rgba(200,255,200,0.5)]">
+            <p className="text-xl text-white/80 max-w-lg mx-auto leading-relaxed mb-12 text-shadow-lg drop-shadow-[0_2px_2px_rgba(0,0,0,1)]">
         Welcome to Assistio AI, where we harness the power of artificial intelligence to
 transform your ideas into reality.
             </p>
